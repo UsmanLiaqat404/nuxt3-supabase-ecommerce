60c752ddcbffc5f991204556114dd6e2b1254818
Usman Liaqat <memerboyemiology444@gmail.com>
2024-03-18 14:49:38 +0500

    Add dependencies and configure Nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/supabase",
+    "@nuxt/ui",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+  ],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+  },
+});
